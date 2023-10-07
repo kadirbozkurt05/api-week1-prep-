58023715840cce406528661ec15a67fa034a2a1f
Kadir Bozkurt <kadirbozkurt05@yandex.com>
2023-10-07 21:13:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const STEP_SIZE_PX = 10;
+const STEP_INTERVAL_MS = 50;
+const DANCE_TIME_MS = 5000;
+const DANCING_CAT_URL = 'https://media1.tenor.com/images/2de63e950fb254920054f9bd081e8157/tenor.gif';
+const WALKING_CAT_URL = 'http://www.anniemation.com/clip_art/images/cat-walk.gif';
+
+function walk(img, startPos, stopPos) {
+  return new Promise((resolve) => {
+    const walkingCat = setInterval(() => {
+      startPos += STEP_SIZE_PX;
+
+      if (startPos < stopPos) {
+        img.style.left = `${startPos}px`;
+      } else {
+        clearInterval(walkingCat);
+        resolve();
+      }
+    }, STEP_INTERVAL_MS);
+  });
+}
+
+function dance(img) {
+  return new Promise((resolve) => {
+    img.src = DANCING_CAT_URL;
+
+    setTimeout(() => {
+      img.src = WALKING_CAT_URL;  
+      resolve();
+    }, DANCE_TIME_MS);
+  });
+}
+
+function catWalk() {
+  const img = document.querySelector('img');
+  const startPos = -img.width;
+  const centerPos = (window.innerWidth - img.width) / 2;
+  const stopPos = window.innerWidth;
+
+  function walkDance() {
+    walk(img, startPos, centerPos)
+      .then(() => dance(img))
+      .then(() => walk(img, centerPos, stopPos))
+      .then(walkDance); 
+  }
+
+  walkDance();
+}
+
+window.addEventListener('load', catWalk);
